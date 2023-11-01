37296675c54fc999384354883b2c647cfe7a1f83
AJ ONeal <aj@therootcompany.com>
2023-11-01 12:13:57 -0600

    feat: checks for memory leaks in debug

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,36 @@
 const std = @import("std");
 
+const builtin = @import("builtin");
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = if (builtin.mode == .Debug) blk: {
+        std.log.info("builtin.mode == .Debug", .{});
+        const GpaAlloc = std.heap.GeneralPurposeAllocator(.{});
+        break :blk GpaAlloc{
+            .backing_allocator = std.heap.page_allocator,
+        };
+    } else blk: {
+        break :blk void{};
+    };
+    defer if (builtin.mode == .Debug) {
+        std.debug.print("Checking for memory leaks...", .{});
+        _ = gpa.deinit();
+        std.debug.print(" OK (all memory deallocated)\n", .{});
+    };
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var allocator = if (builtin.mode == .Debug) blk: {
+        break :blk gpa.allocator();
+    } else blk: {
+        break :blk std.heap.page_allocator;
+    };
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
 
-    try bw.flush(); // don't forget to flush!
-}
+    // cause a memory leak on purpose to observe the leak detector
+    //_ = try allocator.create(u8);
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    while (args.next()) |arg| {
+        std.log.info("{s}", .{arg});
+    }
 }
