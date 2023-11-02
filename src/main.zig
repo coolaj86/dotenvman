319707f191523103e195681a1dc3b3cb156f1336
AJ ONeal <aj@therootcompany.com>
2023-11-01 18:31:05 -0600

    feat: reads the first variable name

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,11 @@ const std = @import("std");
 
 const builtin = @import("builtin");
 
+const HashMap = std.StringHashMapUnmanaged([]const u8);
+const ErrorMessages = struct {
+    const all_caps = "ENVs must begin with 'export ' or ALL_CAPS, not e{s}";
+};
+
 pub fn main() !u8 {
     var gpa = if (builtin.mode == .Debug) blk: {
         std.log.info("builtin.mode == .Debug", .{});
@@ -64,12 +69,104 @@ pub fn main() !u8 {
         std.io.getStdIn();
     defer if (read_file) file.close();
 
-    const reader = file.reader();
-    var b = try reader.readByte();
-    std.log.info("{c}", .{b});
+    var buffered_reader = std.io.BufferedReader(
+        4096,
+        @TypeOf(file.reader()),
+    ){ .unbuffered_reader = file.reader() };
+    const reader = buffered_reader.reader();
+
+    var env = Env{ .allocator = allocator };
+    defer env.deinit();
+    _ = try env.parse(reader);
 
     return 0;
 }
+
+pub const Env = struct {
+    pub const State = enum {
+        start,
+        value,
+        variable,
+        single_quote,
+        double_quote,
+        escape,
+    };
+
+    allocator: std.mem.Allocator,
+    map: HashMap = .{},
+
+    pub fn deinit(env: *Env) void {
+        env.map.deinit(env.allocator);
+    }
+
+    /// caller should provide arena and deinit after use
+    pub fn parse(env: *Env, reader: anytype) !void {
+        var state: State = .start;
+
+        // '#' ignore all until end of line
+        // 'export ' ignore
+        // 'UPPER_SNAKES'
+        var name_buf = std.ArrayListUnmanaged(u8){};
+        defer name_buf.deinit(env.allocator);
+        state_loop: while (true) {
+            // we only handle UTF-8 - because we're sane
+            const char = try reader.readByte();
+            switch (state) {
+                .start => {
+                    switch (char) {
+                        ' ', '\r', '\n', '\t' => {},
+                        '#' => {
+                            while (true) {
+                                const next = try reader.readByte();
+                                switch (next) {
+                                    '\r', '\n' => break,
+                                    else => {},
+                                }
+                            }
+                        },
+                        'e' => {
+                            const chars = try reader.readBytesNoEof(6);
+                            // e'xport '
+                            if (!std.mem.eql(u8, "xport ", &chars)) {
+                                std.log.err(ErrorMessages.all_caps, .{chars});
+                                return error.Failed;
+                            }
+                        },
+                        else => {
+                            name_buf.items.len = 0;
+                            try name_buf.append(env.allocator, char);
+                            while (true) {
+                                const next = try reader.readByte();
+                                switch (next) {
+                                    'A'...'Z' => {
+                                        try name_buf.append(env.allocator, next);
+                                    },
+                                    '=' => {
+                                        state = .value;
+                                        break;
+                                    },
+                                    else => {
+                                        std.log.err(ErrorMessages.all_caps, .{"TODO"});
+                                        return error.Failed;
+                                    },
+                                }
+                            }
+
+                            std.log.info("{s}", .{name_buf.items});
+
+                            // TODO do the rest of the stuff
+
+                            break :state_loop;
+                        },
+                    }
+                },
+                else => {
+                    return error.Failed;
+                },
+            }
+        }
+    }
+};
 
 inline fn openFile(filepath: []const u8, write: bool) error{Failed}!std.fs.File {
     return std.fs.cwd().openFile(
